ea1b4680636d90b932731cded2d3ffeb2263183b
Chris Bushell <cbushell@thoughtworks.com>
2017-02-09 18:25:43 +0000

    handle multiple config files

--- app.rb
+++ app.rb
@@ -4,24 +4,24 @@ require 'sinatra'
 class SimonApp < Sinatra::Base
 
   configure do
-    url_paths = []
+    configs = {}
 
     Dir['config/*.json'].each do |file_name|
-      json = JSON.parse(File.read(file_name))
+      config = JSON.parse(File.read(file_name))
+      config_name = config['name']
 
-      url_path = file_name.gsub(/^config\//, '').gsub(/\.json$/, '')
-      url_paths << url_path
-
-      get "/#{url_path}" do
+      get "/#{config_name}" do
         '<html><body><h1>Foo</h1></body></html>'
       end
+
+      configs[config['name']] = config
     end
 
-    set :url_paths, url_paths
+    set :configs, configs
   end
 
   get '/' do
-    haml :index, :locals => {url_paths: settings.url_paths}
+    haml :index, :locals => {sources: settings.configs.keys}
   end
 
 end
