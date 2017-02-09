480e7e9bba8f1c4d5e8774e1130f16e7b3bf6f54
Chris Bushell <cbushell@thoughtworks.com>
2017-02-09 15:00:01 +0000

    dynamically create routes based on config file

--- app.rb
+++ app.rb
@@ -1,7 +1,27 @@
+require 'json'
 require 'sinatra'
 
 class SimonApp < Sinatra::Base
-  get '/' do
-    haml :index
+
+  configure do
+    url_paths = []
+
+    Dir['config/*.json'].each do |file_name|
+      json = JSON.parse(File.read(file_name))
+
+      url_path = file_name.gsub(/^config\//, '').gsub(/\.json$/, '')
+      url_paths << url_path
+
+      get "/#{url_path}" do
+        '<html><body><h1>Foo</h1></body></html>'
+      end
+    end
+
+    set :url_paths, url_paths
   end
+
+  get '/' do
+    haml :index, :locals => {url_paths: settings.url_paths}
+  end
+
 end
