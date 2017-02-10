bdfe67875e33b35c7033436bc45c7ed1e73a76f2
Chris Bushell <cbushell@thoughtworks.com>
2017-02-10 13:02:33 +0000

    implement sane routing

--- app.rb
+++ app.rb
@@ -9,26 +9,7 @@ class SimonApp < Sinatra::Base
 
     Dir['config/*.json'].each do |file_name|
       config = JSON.parse(File.read(file_name))
-      config_name = config['name']
-
-      get "/#{config_name}" do
-        haml :status, :locals => {config_name: config_name, sources: config['sources'].collect{|s| s['url']}}
-      end
-
-      get "/:config_name/test" do
-        content_type :json
-
-        response = settings.configs[params[:config_name]]['sources'].collect do |source|
-          url = source['url']
-          method = source['method']
-
-          {url: url, responseCode: HTTParty.get(url).response.code}
-        end
-
-        response.to_json
-      end
-
-      configs[config['name']] = config
+      configs[config['name'].downcase.gsub(/\s/, '')] = config
     end
 
     set :configs, configs
@@ -36,6 +17,28 @@ class SimonApp < Sinatra::Base
 
   get '/' do
     haml :index, :locals => {sources: settings.configs.keys}
+  end
+
+  get '/:config_name' do
+    haml :status, :locals => {config_name: params[:config_name],
+                              sources: settings.configs[config_name_as_url(params[:config_name])]['sources'].collect { |s| s['url'] }}
+  end
+
+  get '/:config_name/test' do
+    content_type :json
+
+    settings.configs[params[:config_name]]['sources'].collect do |source|
+      url = source['url']
+      method = source['method']
+
+      {url: url, responseCode: HTTParty.get(url).response.code}
+    end.to_json
+  end
+
+  helpers do
+    def config_name_as_url(config_name)
+      config_name.downcase.gsub(/\s/, '')
+    end
   end
 
 end
