ad6b776b08c53fe8c6be1dabcfc2ef0c99ae043e
Chris Bushell <cbushell@thoughtworks.com>
2017-02-10 14:35:55 +0000

    rename

--- app.rb
+++ app.rb
@@ -1,28 +1,28 @@
 require 'json'
 require 'sinatra'
 require 'httparty'
-require_relative 'lib/configs'
+require_relative 'lib/simon_config'
 require_relative 'lib/service_test'
 
 class SimonApp < Sinatra::Base
 
   configure do
-    set :configs, Configs.new('config')
+    set :config, SimonConfig.new('config')
   end
 
   get '/' do
-    haml :index, :locals => {sources: settings.configs.config_names}
+    haml :index, :locals => {sources: settings.config.config_names}
   end
 
   get '/:config_name_as_url' do
-    config = settings.configs.config_for(params[:config_name_as_url])
+    config = settings.config.config_for(params[:config_name_as_url])
     haml :status, :locals => {config: config}
   end
 
   get '/:config_name_as_url/test' do
     content_type :json
 
-    config = settings.configs.config_for(params[:config_name_as_url])
+    config = settings.config.config_for(params[:config_name_as_url])
     ServiceTest.new(config).test_services.to_json
   end
 
--- lib/simon_config.rb
+++ lib/simon_config.rb
@@ -1,4 +1,4 @@
-class Configs
+class SimonConfig
 
   def initialize(dir)
     @configs = {}
