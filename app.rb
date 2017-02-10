74f0e46ff04dc9960b3f906f54924df2abe3447d
Chris Bushell <cbushell@thoughtworks.com>
2017-02-10 14:51:02 +0000

    massively simplify

--- app.rb
+++ app.rb
@@ -1,29 +1,23 @@
 require 'json'
 require 'sinatra'
 require 'httparty'
-require_relative 'lib/simon_config'
 require_relative 'lib/service_test'
 
 class SimonApp < Sinatra::Base
 
   configure do
-    set :config, SimonConfig.new('config')
+    config_filename = ENV['SIMON_CONFIG'] || 'config/news.json'
+    
+    set :config, JSON.parse(File.read(config_filename))
   end
 
   get '/' do
-    haml :index, :locals => {sources: settings.config.config_names}
+    haml :index, :locals => {config: settings.config}
   end
 
-  get '/:config_name_as_url' do
-    config = settings.config.config_for(params[:config_name_as_url])
-    haml :status, :locals => {config: config}
-  end
-
-  get '/:config_name_as_url/test' do
+  get '/test' do
     content_type :json
-
-    config = settings.config.config_for(params[:config_name_as_url])
-    ServiceTest.new(config).test_services.to_json
+    ServiceTest.new(settings.config).test_services.to_json
   end
 
 end
