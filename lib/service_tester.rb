e3f275cd271758e3fb3273426ff0f27110f347b2
Chris Bushell <cbushell@thoughtworks.com>
2017-02-12 18:44:37 +0000

    add support for http post

--- lib/service_tester.rb
+++ lib/service_tester.rb
@@ -8,17 +8,29 @@ class ServiceTester
   def test_services
     @config['sources'].collect do |source|
       url = source['url']
-      method = source['method']
+      method = source['httpMethod']
 
       startTime = Time.now
-      response = HTTParty.get(url).response
 
-      @service_stats[url].update_stats(response)
+      if method == 'GET'
+        response = HTTParty.get(url).response
 
-      {url: url,
-       responseCode: response.code,
-       responseTime: Time.now - startTime}.
-          merge(@service_stats[url].stats_hash)
+        @service_stats[url].update_stats(response)
+
+        {url: url,
+         responseCode: response.code,
+         responseTime: Time.now - startTime}.
+            merge(@service_stats[url].stats_hash)
+      else
+        response = HTTParty.post(url, source['data']).response
+
+        @service_stats[url].update_stats(response)
+
+        {url: url,
+         responseCode: response.code,
+         responseTime: Time.now - startTime}.
+            merge(@service_stats[url].stats_hash)
+      end
 
     end
   end
